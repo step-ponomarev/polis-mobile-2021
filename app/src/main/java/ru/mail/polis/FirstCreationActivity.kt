51d72601cd2de40c2d1432d9ba698f4eb4eaa21f
OGSegu <seguisreal@gmail.com>
2021-04-02 16:24:54 +0300

    Raw version of first advert creation activity

---
+++ app/src/main/java/ru/mail/polis/FirstCreationActivity.kt
@@ -0,0 +1,11 @@
+package ru.mail.polis
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class FirstCreationActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_first_creation)
+    }
+}
