68eba245a926865afe432cc0d695081721c675e3
OGSegu <seguisreal@gmail.com>
2021-04-02 21:26:51 +0300

    Second screen has been done without icons

---
+++ app/src/main/java/ru/mail/polis/CreationActivity.kt
@@ -0,0 +1,11 @@
+package ru.mail.polis
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class CreationActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_advert_creation)
+    }
+}
