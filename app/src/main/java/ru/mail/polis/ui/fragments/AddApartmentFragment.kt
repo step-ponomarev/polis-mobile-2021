a7679dc6fb262dec3355f1c1090fbba3c0ca44e7
IlyaAAAA <ilya-sachuk@mail.ru>
2021-04-05 00:50:03 +0300

    added onCreateView

--- app/src/main/java/ru/mail/polis/ui/fragments/AddApartmentFragment.kt
+++ app/src/main/java/ru/mail/polis/ui/fragments/AddApartmentFragment.kt
@@ -1,6 +1,20 @@
 package ru.mail.polis.ui.fragments
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import ru.mail.polis.R
 
 class AddApartmentFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_add_apartment, container, false)
+    }
+
 }
