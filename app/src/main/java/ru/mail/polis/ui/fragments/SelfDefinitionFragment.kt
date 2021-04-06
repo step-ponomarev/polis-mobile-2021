1f3bbc2d2e62d82c7ce376f8222248634a1c4458
IlyaAAAA <ilya-sachuk@mail.ru>
2021-04-06 14:43:55 +0300

    formated

--- app/src/main/java/ru/mail/polis/ui/fragments/SelfDefinitionFragment.kt
+++ app/src/main/java/ru/mail/polis/ui/fragments/SelfDefinitionFragment.kt
@@ -10,7 +10,8 @@ import ru.mail.polis.R
 class SelfDefinitionFragment : Fragment() {
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.fragment_self_definition, container, false)
