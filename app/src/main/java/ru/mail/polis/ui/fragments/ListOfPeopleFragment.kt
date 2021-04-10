fb4173787c7d118f75abb8f21a452870c215b4cd
NickPeut <veropeu@mail.ru>
2021-04-10 21:38:15 +0300

    add Fragment and rooms

--- app/src/main/java/ru/mail/polis/ui/fragments/ListOfPeopleFragment.kt
+++ app/src/main/java/ru/mail/polis/ui/fragments/ListOfPeopleFragment.kt
@@ -18,9 +18,9 @@ class ListOfPeopleFragment : Fragment() {
     }
 
     override fun onCreateView(
-            inflater: LayoutInflater,
-            container: ViewGroup?,
-            savedInstanceState: Bundle?
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.list_of_people, container, false)
     }
@@ -38,33 +38,34 @@ class ListOfPeopleFragment : Fragment() {
 
 private fun generateTestPeopleList(): List<Person> {
     return listOf(
-            Person(
-                    R.drawable.photo1,
-                    "Степан Пономарев",
-                    "22 года",
-                    R.mipmap.ic_mark_foreground,
-                    listOf(R.drawable.ic_ciggarete_svg_image,
-                            R.drawable.ic_kid_svg_image,
-                            R.drawable.ic_paw_svg_image,
-                            R.drawable.ic_drum_svg_image
-                    ),
-                    "Купчино",
-                    0,
-                    Pair(20000, 35000),
-                    listOf("1 комната", "2 комнаты"),
-                    "Привет, меня зовут Степа и я не алкоголик. У меня есть ребенок и жена ищем квартиру для длительного проживания. У нас четыре щеночка и барабанная установка"
+        Person(
+            R.drawable.photo1,
+            "Степан Пономарев",
+            "22 года",
+            R.mipmap.ic_mark_foreground,
+            listOf(
+                R.drawable.ic_ciggarete_svg_image,
+                R.drawable.ic_kid_svg_image,
+                R.drawable.ic_paw_svg_image,
+                R.drawable.ic_drum_svg_image
             ),
-            Person(
-                    R.drawable.ic_account_circle_24,
-                    "Ника Пеутина",
-                    "19 лет",
-                    R.mipmap.ic_mark_foreground,
-                    listOf(R.drawable.ic_paw_svg_image),
-                    "Девяткино",
-                    0,
-                    Pair(10000, 25000),
-                    listOf("3 комнаты"),
-                    "Привет, меня зовут Ника. У меня три кота. Со мной будет жить бабушка, мама, отец, два брата и маленькая сестра. Ищем квартиру для длительного проживания."
-            )
+            "Купчино",
+            0,
+            Pair(20000, 35000),
+            listOf("1 комната", "2 комнаты"),
+            "Привет, меня зовут Степа и я не алкоголик. У меня есть ребенок и жена ищем квартиру для длительного проживания. У нас четыре щеночка и барабанная установка"
+        ),
+        Person(
+            R.drawable.ic_account_circle_24,
+            "Ника Пеутина",
+            "19 лет",
+            R.mipmap.ic_mark_foreground,
+            listOf(R.drawable.ic_paw_svg_image),
+            "Девяткино",
+            0,
+            Pair(10000, 25000),
+            listOf("3 комнаты"),
+            "Привет, меня зовут Ника. У меня три кота. Со мной будет жить бабушка, мама, отец, два брата и маленькая сестра. Ищем квартиру для длительного проживания."
+        )
     )
 }
