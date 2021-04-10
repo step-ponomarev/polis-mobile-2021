d53f081de7987883fbbed1555941d7a455d4baf9
Stepan Ponomarev <step.ponomarev@gmail.com>
2021-04-10 20:41:06 +0300

    added fragment

---
+++ app/src/main/java/ru/mail/polis/ui/fragments/ProposedApartmentsFragment.kt
@@ -0,0 +1,18 @@
+package ru.mail.polis.ui.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import ru.mail.polis.R
+
+class ProposedApartmentsFragment: Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_proposed_apartment_list, container, false)
+    }
+}
