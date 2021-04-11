adfa545762a9dde419a5db9eace45008e86f2975
Stepan Ponomarev <step.ponomarev@gmail.com>
2021-04-11 14:41:45 +0300

    added nav

--- app/src/main/java/ru/mail/polis/MainActivity.kt
+++ app/src/main/java/ru/mail/polis/MainActivity.kt
@@ -4,17 +4,21 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.commit
 import androidx.fragment.app.replace
+import androidx.navigation.Navigation.findNavController
 import ru.mail.polis.ui.fragments.LoginFragment
 
 class MainActivity : AppCompatActivity() {
+    override fun onSupportNavigateUp() =
+        findNavController(this, R.id.navHostFragment).navigateUp()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        supportFragmentManager.commit {
-            replace<LoginFragment>(R.id.fragment_container_view)
-            setReorderingAllowed(true)
-            addToBackStack(LoginFragment.NAME)
-        }
+//        supportFragmentManager.commit {
+//            replace<LoginFragment>(R.id.fragment_container_view)
+//            setReorderingAllowed(true)
+//            addToBackStack(LoginFragment.NAME)
+//        }
     }
 }
