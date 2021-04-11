504b1830b0ca3ad40af1ea6bb5eaec7bfbdb4490
IlyaAAAA <ilya-sachuk@mail.ru>
2021-04-11 22:17:01 +0300

    almos all

--- app/src/main/java/ru/mail/polis/ui/fragments/AddApartmentFragment.kt
+++ app/src/main/java/ru/mail/polis/ui/fragments/AddApartmentFragment.kt
@@ -4,10 +4,15 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.Spinner
 import androidx.fragment.app.Fragment
 import ru.mail.polis.R
 
+
 class AddApartmentFragment : Fragment() {
+
+    private lateinit var spinner: Spinner
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -17,4 +22,18 @@ class AddApartmentFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_add_apartment, container, false)
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        spinner = view.findViewById(R.id.fragment_add_apartment_spinner)
+
+        ArrayAdapter.createFromResource(
+            requireContext(),
+            R.array.subways,
+            android.R.layout.simple_spinner_item
+        ).also {
+            it.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            spinner.adapter = it
+        }
+    }
 }
