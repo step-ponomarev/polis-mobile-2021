632bd71b60d4cee331dad153f697b500c7c1bfa6
IlyaAAAA <ilya-sachuk@mail.ru>
2021-04-11 19:55:04 +0300

    added navigation to add apart

--- app/src/main/java/ru/mail/polis/ui/fragments/SelfDefinitionFragment.kt
+++ app/src/main/java/ru/mail/polis/ui/fragments/SelfDefinitionFragment.kt
@@ -4,10 +4,14 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import ru.mail.polis.R
 
 class SelfDefinitionFragment : Fragment() {
+
+    private lateinit var addApartmentButton: Button
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -15,5 +19,16 @@ class SelfDefinitionFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.fragment_self_definition, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        addApartmentButton = view.findViewById(R.id.fragment_self_definition__button_rent_apartment)
+        addApartmentButton.setOnClickListener(this::onClickAddApartment)
+    }
+
+    private fun onClickAddApartment(view: View) {
+        findNavController().navigate(R.id.nav_graph__add_apartment_fragment)
     }
 }
