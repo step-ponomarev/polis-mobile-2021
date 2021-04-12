ae0d051b6fa9813be26528f39ed45a633c72b921
OGSegu <seguisreal@gmail.com>
2021-04-12 16:03:44 +0300

    Some design changes

--- app/src/main/java/ru/mail/polis/ui/fragments/CreationFragment.kt
+++ app/src/main/java/ru/mail/polis/ui/fragments/CreationFragment.kt
@@ -1,18 +1,18 @@
-package ru.mail.polis
+package ru.mail.polis.ui.fragments
 
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
+import ru.mail.polis.R
 
-class FirstCreationFragment : Fragment() {
+class CreationFragment : Fragment() {
     override fun onCreateView(
             inflater: LayoutInflater,
             container: ViewGroup?,
             savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_first_creation, container, false)
+        return inflater.inflate(R.layout.fragment_advert_creation, container, false)
     }
 }
