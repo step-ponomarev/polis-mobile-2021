3b804b6d0d81daea4db01690ffcb1eaaa3a07346
OGSegu <seguisreal@gmail.com>
2021-04-12 15:54:35 +0300

    Activity was transformed to fragments

---
+++ app/src/main/java/ru/mail/polis/FirstCreationFragment.kt
@@ -0,0 +1,18 @@
+package ru.mail.polis
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+
+class FirstCreationFragment : Fragment() {
+    override fun onCreateView(
+            inflater: LayoutInflater,
+            container: ViewGroup?,
+            savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_first_creation, container, false)
+    }
+}
