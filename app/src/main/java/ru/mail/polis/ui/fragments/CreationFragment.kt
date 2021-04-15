6bb9906ad3c3bb9adcb699fe0a2088901250ea45
OGSegu <seguisreal@gmail.com>
2021-04-16 01:26:40 +0300

    Added enums to spinner

--- app/src/main/java/ru/mail/polis/ui/fragments/CreationFragment.kt
+++ app/src/main/java/ru/mail/polis/ui/fragments/CreationFragment.kt
@@ -4,8 +4,11 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.Spinner
 import androidx.fragment.app.Fragment
 import ru.mail.polis.R
+import ru.mail.polis.metro.Metro
 
 class CreationFragment : Fragment() {
 
@@ -15,5 +18,16 @@ class CreationFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.fragment_advert_creation, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        val spinner = view.findViewById<Spinner>(R.id.fragment_add_apartment_spinner)
+        val adapter = ArrayAdapter(
+            requireContext(),
+            android.R.layout.simple_list_item_1,
+            Metro.values()
+        )
+        spinner.adapter = adapter
+        return super.onViewCreated(view, savedInstanceState)
     }
 }
