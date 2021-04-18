0018c1d47ef6bcfd2846674a3402adfeac6a6afc
IlyaAAAA <ilya-sachuk@mail.ru>
2021-04-18 14:14:45 +0300

    added changing circle color

--- app/src/main/java/ru/mail/polis/ui/fragments/AddApartmentFragment.kt
+++ app/src/main/java/ru/mail/polis/ui/fragments/AddApartmentFragment.kt
@@ -4,9 +4,8 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ArrayAdapter
-import android.widget.Button
-import android.widget.Spinner
+import android.widget.*
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import ru.mail.polis.R
@@ -15,8 +14,9 @@ import ru.mail.polis.metro.Metro
 class AddApartmentFragment : Fragment() {
 
     private lateinit var spinner: Spinner
-    private val metroList = Metro.values()
     private lateinit var addApartmentButton: Button
+    private lateinit var metroCircleIv: ImageView
+    private val metroList = Metro.values()
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -30,6 +30,7 @@ class AddApartmentFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         spinner = view.findViewById(R.id.fragment_add_apartment_spinner)
+        metroCircleIv = view.findViewById(R.id.fragment_add_apartment__circle)
 
         val metroNamesList = metroList.map { it.stationName }
 
@@ -38,6 +39,26 @@ class AddApartmentFragment : Fragment() {
                 it.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
                 spinner.adapter = it
             }
+
+        spinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener{
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+
+
+            }
+
+            override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+                val metro = Metro.values()[position]
+                if (view != null) {
+                    metroCircleIv.background.setTint(
+                        ContextCompat.getColor(
+                            view.context,
+                            metro.branchColor
+                        )
+                    )
+                }
+            }
+
+        }
 
         addApartmentButton = view.findViewById(R.id.add_button)
         addApartmentButton.setOnClickListener(this::onClickAddApartment)
