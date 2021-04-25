b7d50a834add41ed30cf791b9b9135d7a3d4a5c5
Stepan Ponomarev <step.ponomarev@gmail.com>
2021-04-25 19:24:31 +0300

    added aparts service

---
+++ app/src/androidTest/java/ApartmentServiceTest.kt
@@ -0,0 +1,70 @@
+import kotlinx.coroutines.runBlocking
+import org.junit.Assert
+import org.junit.Test
+import ru.mail.polis.dao.ApartmentED
+import ru.mail.polis.dao.ApartmentService
+import ru.mail.polis.dao.IApartmentService
+
+class ApartmentServiceTest {
+    private val apartmentService: IApartmentService = ApartmentService()
+
+    @Test
+    fun apartmentShouldBeAddedSuccessful() {
+        runBlocking {
+            val apartment = createTestApartment()
+
+            val addedPerson = apartmentService.addApartment(apartment)
+            val gotPerson = apartmentService.findByEmail(addedPerson.email)
+
+            Assert.assertNotNull(gotPerson)
+            Assert.assertEquals(addedPerson, gotPerson)
+
+            apartmentService.deleteApartmentByEmail(addedPerson.email)
+        }
+    }
+
+    @Test
+    fun deletingShouldBeSuccessful() {
+        runBlocking {
+            val apartment = createTestApartment()
+
+            val addedPerson = apartmentService.addApartment(apartment)
+            apartmentService.deleteApartmentByEmail(addedPerson.email)
+
+            val gotPerson = apartmentService.findByEmail(addedPerson.email)
+
+            Assert.assertNull(gotPerson)
+        }
+    }
+
+    @Test
+    fun updatingShouldBeSuccessful() {
+        runBlocking {
+            val apartment = createTestApartment()
+            val updatedApartment = createUpdatedApartment(apartment)
+
+            val addedApartment = apartmentService.addApartment(apartment)
+            apartmentService.updateApartment(updatedApartment)
+            val gotApartment = apartmentService.findByEmail(apartment.email)
+
+            Assert.assertEquals(gotApartment, updatedApartment)
+
+            apartmentService.deleteApartmentByEmail(addedApartment.email)
+        }
+    }
+
+    private fun createTestApartment(): ApartmentED {
+        return ApartmentED(
+            "test@test.test", "test", "test", 0,
+            null, null, null, null, null
+        )
+    }
+
+    private fun createUpdatedApartment(oldApartment: ApartmentED): ApartmentED {
+        return ApartmentED(
+            oldApartment.email, "${oldApartment.ownerAvatar}1", "${oldApartment.ownerName}1",
+            null, null, null,
+            null, null, null
+        )
+    }
+}
---
+++ app/src/main/java/ru/mail/polis/dao/ApartmentService.kt
@@ -0,0 +1,121 @@
+package ru.mail.polis.dao
+
+import android.util.Log
+import com.google.firebase.firestore.CollectionReference
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
+import kotlinx.coroutines.suspendCancellableCoroutine
+import kotlin.coroutines.resume
+import kotlin.coroutines.resumeWithException
+
+class ApartmentService : IApartmentService {
+    private val db: FirebaseFirestore = Firebase.firestore
+    private val apartmentCollection: CollectionReference =
+        db.collection(Collections.APARTMENT.collectionName)
+
+    override suspend fun findByEmail(email: String): ApartmentED? {
+        val apartmentRef = apartmentCollection.document(email)
+
+        return suspendCancellableCoroutine { coroutine ->
+            apartmentRef.get()
+                .addOnSuccessListener {
+                    coroutine.resume(it.toObject(ApartmentED::class.java))
+                }
+                .addOnFailureListener {
+                    coroutine.resumeWithException(
+                        RuntimeException("Filed fetching apartment with email: $email", it)
+                    )
+                }
+        }
+    }
+
+    override suspend fun findAll(): List<ApartmentED> {
+        return suspendCancellableCoroutine { coroutine ->
+            apartmentCollection.get()
+                .addOnSuccessListener {
+                    coroutine.resume(it.toObjects(ApartmentED::class.java))
+                }
+                .addOnFailureListener {
+                    coroutine.resumeWithException(
+                        RuntimeException("Filed fetching apartment list", it)
+                    )
+                }
+        }
+    }
+
+    override suspend fun addApartment(apartment: ApartmentED): ApartmentED {
+        return suspendCancellableCoroutine { coroutine ->
+            apartmentCollection.document(apartment.email)
+                .set(apartment)
+                .addOnFailureListener {
+                    coroutine.resumeWithException(
+                        RuntimeException(
+                            "Failure adding apartments with email: ${apartment.email}",
+                            it
+                        )
+                    )
+                }
+                .addOnSuccessListener {
+                    Log.i(
+                        this::class.java.name,
+                        "Successful adding apartments with email: ${apartment.email}"
+                    )
+
+                    coroutine.resume(apartment)
+                }
+        }
+    }
+
+    override suspend fun updateApartment(apartment: ApartmentED): ApartmentED {
+        val apartmentRef = apartmentCollection.document(apartment.email)
+
+        return suspendCancellableCoroutine { coroutine ->
+            apartmentRef.update(apartmentToMap(apartment))
+                .addOnFailureListener {
+                    coroutine.resumeWithException(
+                        RuntimeException(
+                            "Failure updating person with email: ${apartment.email}",
+                            it
+                        )
+                    )
+                }
+                .addOnSuccessListener {
+                    coroutine.resume(apartment)
+                }
+        }
+    }
+
+    override suspend fun deleteApartmentByEmail(email: String) {
+        val apartmentRef = apartmentCollection.document(email)
+
+        return suspendCancellableCoroutine { coroutine ->
+            apartmentRef.delete()
+                .addOnSuccessListener {
+                    coroutine.resume(Unit)
+                }
+                .addOnFailureListener {
+                    coroutine.resumeWithException(
+                        RuntimeException(
+                            "Failure deleting apartment with email: $email",
+                            it
+                        )
+                    )
+                }
+        }
+    }
+
+    private fun apartmentToMap(apartment: ApartmentED): Map<String, Any?> {
+        return mapOf(
+            "email" to apartment.email,
+            "ownerAvatar" to apartment.ownerAvatar,
+            "ownerName" to apartment.ownerName,
+            "ownerAge" to apartment.ownerAge,
+            "metro" to apartment.metro,
+            "roomCount" to apartment.roomCount,
+            "apartmentSquare" to apartment.apartmentSquare,
+            "apartmentCosts" to apartment.apartmentCosts,
+            "photosUrls" to apartment.photosUrls
+        )
+    }
+}
