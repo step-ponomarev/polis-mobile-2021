7f0e739c92e57ddd627be9eaf35eda9fab102b8c
IlyaAAAA <ilya-sachuk@mail.ru>
2021-05-08 19:17:22 +0300

    deleted wildcard import

--- app/src/main/java/ru/mail/polis/ui/fragments/AddApartmentFragment.kt
+++ app/src/main/java/ru/mail/polis/ui/fragments/AddApartmentFragment.kt
@@ -9,7 +9,14 @@ import android.provider.MediaStore
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.*
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Button
+import android.widget.EditText
+import android.widget.ImageButton
+import android.widget.ImageView
+import android.widget.LinearLayout
+import android.widget.Spinner
 import androidx.activity.result.ActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.constraintlayout.widget.ConstraintLayout
