63ac58fce0504f2ef68fc49964441dceeea8c263
IlyaAAAA <ilya-sachuk@mail.ru>
2021-05-19 15:36:11 +0300

    added converter

--- app/src/main/java/ru/mail/polis/viewModels/AddApartmentViewModel.kt
+++ app/src/main/java/ru/mail/polis/viewModels/AddApartmentViewModel.kt
@@ -5,18 +5,14 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
+import ru.mail.polis.converter.Converter
 import ru.mail.polis.dao.IPhotoUriService
 import ru.mail.polis.dao.PhotoUriService
 import ru.mail.polis.dao.apartments.ApartmentED
 import ru.mail.polis.dao.apartments.ApartmentService
 import ru.mail.polis.dao.apartments.IApartmentService
-import java.io.ByteArrayOutputStream
 
 class AddApartmentViewModel : ViewModel() {
-
-    companion object {
-        private const val BITMAP_QUALITY = 100
-    }
 
     private val apartmentService: IApartmentService = ApartmentService.getInstance()
     private val photoUriService: IPhotoUriService = PhotoUriService()
@@ -30,17 +26,11 @@ class AddApartmentViewModel : ViewModel() {
         }
     }
 
-    private fun bitmapToInputStream(bitmap: Bitmap): ByteArray {
-        val baos = ByteArrayOutputStream()
-        bitmap.compress(Bitmap.CompressFormat.JPEG, BITMAP_QUALITY, baos)
-        return baos.toByteArray()
-    }
-
     private suspend fun getUrlList(): List<String> {
         val urlList = ArrayList<String>()
 
         list.forEach {
-            val url = photoUriService.saveImage(bitmapToInputStream(it))
+            val url = photoUriService.saveImage(Converter.bitmapToInputStream(it))
             urlList.add(url.toString())
         }
 
