ce748c80871979a1e3a717af3ccf92277a4cc1a9
Stepan Ponomarev <step.ponomarev@gmail.com>
2021-05-22 23:55:55 +0300

    changed

--- app/src/main/java/ru/mail/polis/ui/fragments/AdvertCreationFragment.kt
+++ app/src/main/java/ru/mail/polis/ui/fragments/AdvertCreationFragment.kt
@@ -8,10 +8,13 @@ import android.view.ViewGroup
 import android.widget.ArrayAdapter
 import android.widget.Button
 import android.widget.EditText
+import android.widget.ImageView
 import android.widget.Spinner
+import android.widget.TextView
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
+import com.bumptech.glide.Glide
 import com.google.android.material.chip.Chip
 import com.google.android.material.chip.ChipGroup
 import kotlinx.coroutines.Dispatchers
@@ -19,6 +22,7 @@ import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
 import ru.mail.polis.R
 import ru.mail.polis.dao.person.PersonED
+import ru.mail.polis.dao.users.UserED
 import ru.mail.polis.metro.Metro
 import ru.mail.polis.room.RoomCount
 import ru.mail.polis.viewModels.AdvertCreationViewModel
@@ -27,8 +31,14 @@ import java.util.Collections
 class AdvertCreationFragment : Fragment() {
     private val viewModel = AdvertCreationViewModel()
 
+    private lateinit var email: String
+    private lateinit var user: UserED
+
     private lateinit var spinner: Spinner
     private lateinit var chipGroup: ChipGroup
+    private lateinit var avatarImageView: ImageView
+    private lateinit var nameTextView: TextView
+    private lateinit var ageTextView: TextView
     private lateinit var costFromEditText: EditText
     private lateinit var costToEditText: EditText
     private lateinit var aboutMeEditText: EditText
@@ -58,6 +68,22 @@ class AdvertCreationFragment : Fragment() {
         costToEditText = view.findViewById(R.id.fragment_advert_creation__to_price_et)
         aboutMeEditText = view.findViewById(R.id.fragment_advert_creation__about_me_et)
         createAdvertFragment = view.findViewById(R.id.fragment_advert_creation__continue_btn)
+        avatarImageView = view.findViewById(R.id.component_person_header__avatar)
+        nameTextView = view.findViewById(R.id.component_person_header__name)
+        ageTextView = view.findViewById(R.id.component_person_header__age)
+
+        email = getEmail()
+        GlobalScope.launch(Dispatchers.Main) {
+            user = viewModel.fetchUser(email)
+                ?: throw IllegalStateException("User not found by email: $email")
+
+            if (user.photo != null) {
+                Glide.with(avatarImageView).load(user.photo).into(avatarImageView)
+            }
+
+            nameTextView.text = "${user.name} ${user.surname}"
+            ageTextView.text = user.age.toString()
+        }
 
         createAdvertFragment.setOnClickListener(this::createAdvert)
     }
@@ -69,7 +95,6 @@ class AdvertCreationFragment : Fragment() {
             return
         }
 
-        val email = getEmail()
         val metro = spinner.selectedItem.toString()
         val roomCount = selectedChip.text.toString()
         val costFrom = costFromEditText.text.toString()
@@ -86,9 +111,6 @@ class AdvertCreationFragment : Fragment() {
         }
 
         GlobalScope.launch(Dispatchers.Main) {
-            val user = viewModel.fetchUser(email)
-                ?: throw IllegalStateException("User not found by email: $email")
-
             val person = PersonED.Builder.createBuilder()
                 .email(email)
                 .age(user.age!!)
