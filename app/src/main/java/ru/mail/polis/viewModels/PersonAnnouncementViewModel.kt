83d04b5484f9edfff5da07e0415e30393a2baafb
IlyaAAAA <ilya-sachuk@mail.ru>
2021-05-24 18:02:57 +0300

    added offering apartment

---
+++ app/src/main/java/ru/mail/polis/viewModels/PersonAnnouncementViewModel.kt
@@ -0,0 +1,41 @@
+package ru.mail.polis.viewModels
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import ru.mail.polis.dao.apartments.ApartmentService
+import ru.mail.polis.dao.apartments.IApartmentService
+import ru.mail.polis.dao.propose.IProposeService
+import ru.mail.polis.dao.propose.ProposeED
+import ru.mail.polis.dao.propose.ProposeService
+import ru.mail.polis.dao.propose.ProposeStatus
+
+class PersonAnnouncementViewModel : ViewModel() {
+
+    private val apartmentService: IApartmentService = ApartmentService.getInstance()
+    private val proposeService: IProposeService = ProposeService()
+
+    fun offerApartment(emailFromOffer: String, emailToOffer: String, callback: () -> Unit) {
+        viewModelScope.launch(Dispatchers.IO) {
+
+            val apartment = withContext(Dispatchers.IO) {
+                apartmentService.findByEmail(emailFromOffer)
+            } ?: throw IllegalStateException("You have not got an apartment yet!")
+
+            withContext(Dispatchers.IO) {
+                proposeService.createPropose(
+                    ProposeED(
+                        emailFromOffer,
+                        emailToOffer,
+                        ProposeStatus.PENDING
+                    )
+                )
+                withContext(Dispatchers.Main) {
+                    callback.invoke()
+                }
+            }
+        }
+    }
+}
