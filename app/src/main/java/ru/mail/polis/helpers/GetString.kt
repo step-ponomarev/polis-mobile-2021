2fdab5e848e8879f9a64bb90634f9ca01a8784cf
IlyaAAAA <ilya-sachuk@mail.ru>
2021-06-05 00:36:43 +0300

    fixed

--- app/src/main/java/ru/mail/polis/helpers/GetString.kt
+++ app/src/main/java/ru/mail/polis/helpers/GetString.kt
@@ -6,16 +6,16 @@ import ru.mail.polis.R
 fun getAgeString(age: Long, context: Context): String {
     return when {
         age % 100 in 5..20 -> {
-            "$age" + context.getString(R.string.age_advert_let)
+            "$age " + context.getString(R.string.age_advert_let)
         }
         age % 10 in 2..4 -> {
-            "$age" + context.getString(R.string.age_advert_goda)
+            "$age " + context.getString(R.string.age_advert_goda)
         }
         age % 10 == 1L -> {
-            "$age" + context.getString(R.string.age_advert_god)
+            "$age " + context.getString(R.string.age_advert_god)
         }
         else -> {
-            "$age" + context.getString(R.string.age_advert_let)
+            "$age " + context.getString(R.string.age_advert_let)
         }
     }
 }
