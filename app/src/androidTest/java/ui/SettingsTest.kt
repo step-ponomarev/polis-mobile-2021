fa583d8529fc75649156ebabe89f9e921b8312ff
IlyaAAAA <ilya-sachuk@mail.ru>
2021-11-06 18:09:50 +0300

    changed test

---
+++ app/src/androidTest/java/ui/SettingsTest.kt
@@ -0,0 +1,80 @@
+package ui
+
+import androidx.test.rule.ActivityTestRule
+import com.kaspersky.kaspresso.testcases.api.testcase.TestCase
+import org.junit.Rule
+import org.junit.Test
+import ru.mail.polis.MainActivity
+import ui.data.User
+import ui.screens.ListOfPeopleScreen
+import ui.screens.LoginScreen
+import ui.screens.SettingsScreen
+
+class SettingsTest : TestCase() {
+    //TODO чекнуть scenario и мб заюзать их
+    @get:Rule
+    val activityTestRule = ActivityTestRule(MainActivity::class.java, true, false)
+
+    private val defaultUserInfo: User = User(
+        name = "Иван",
+        surname = "Иванов",
+        phone = "89025563557",
+        age = 38
+    )
+
+    private val testUser: User = User(
+        name = "Артем",
+        surname = "Петров",
+        phone = "89082122231",
+        age = 45
+    )
+
+    @Test
+    fun test() {
+        before {
+            activityTestRule.launchActivity(null)
+
+        }.after {
+            SettingsScreen {
+                changeUserInfo(defaultUserInfo)
+            }
+        }.run {
+            step("Login and navigate to ListOfPeople") {
+                LoginScreen {
+                    loginButton {
+                        isVisible()
+                        click()
+                    }
+                }
+            }
+
+            step("Navigate to settings") {
+                ListOfPeopleScreen {
+                    navigateToSettings()
+                }
+            }
+
+            step("Check current user, change user info and navigate to ListOfPeople") {
+                SettingsScreen {
+                    checkUserInfo(defaultUserInfo)
+                    changeUserInfo(testUser)
+
+                    navigateToListOfPeople()
+                }
+            }
+
+            step("Navigate to Settings") {
+                ListOfPeopleScreen {
+                    navigateToSettings()
+                }
+            }
+
+            step("Check that user info was changed") {
+                SettingsScreen {
+                    checkUserInfo(testUser)
+                }
+            }
+
+        }
+    }
+}
---
+++ app/src/androidTest/java/ui/data/User.kt
@@ -0,0 +1,8 @@
+package ui.data
+
+data class User(
+    val name: String,
+    val surname: String,
+    val phone: String,
+    val age: Int
+)
